05e852cbcd54ad12d14b2b1462805552c7106c9c
Ozan <Jonas05thomsen@gmail.com>
2024-12-19 21:31:30 +0100

    Update10

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,12 +1,16 @@
 import { PrismaClient } from '@prisma/client'
 
-const prismaGlobal = global as typeof global & {
-  prisma?: PrismaClient
+const globalForPrisma = global as unknown as {
+  prisma: PrismaClient | undefined
 }
 
-export const prisma = prismaGlobal.prisma || new PrismaClient()
+export const prisma = globalForPrisma.prisma ?? new PrismaClient({
+  datasources: {
+    db: {
+      url: process.env.POSTGRES_PRISMA_URL
+    }
+  }
+})
 
-if (process.env.NODE_ENV !== 'production') {
-  prismaGlobal.prisma = prisma
-}
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
 
