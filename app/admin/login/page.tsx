ab4217e35ac5266cf19fc4bea482f5151e6a2ee1
Ozan <Jonas05thomsen@gmail.com>
2024-12-20 12:42:27 +0100

    Update

--- app/admin/login/page.tsx
+++ app/admin/login/page.tsx
@@ -94,6 +94,7 @@ export default function AdminLogin() {
               name="username"
               required
               disabled={isLoading}
+              autoComplete="username"
               className="w-full px-4 py-2 rounded-lg bg-white/10 border border-purple-500/30 text-white focus:outline-none focus:border-purple-500"
             />
           </div>
@@ -107,6 +108,7 @@ export default function AdminLogin() {
               name="password"
               required
               disabled={isLoading}
+              autoComplete="current-password"
               className="w-full px-4 py-2 rounded-lg bg-white/10 border border-purple-500/30 text-white focus:outline-none focus:border-purple-500"
             />
           </div>
